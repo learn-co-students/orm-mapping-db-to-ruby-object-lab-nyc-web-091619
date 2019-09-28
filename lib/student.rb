fc8a491f11c164030150957b8adb2f87ad4d0a14
flatironschool <flatironschool@flatironschools-MacBook-Air.local>
2019-09-27 21:34:51 -0400

    Student passes all tests

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 class Student
+
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
@@ -26,6 +27,7 @@ class Student
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
+
   end
 
   def self.find_by_name(name)
@@ -42,18 +44,22 @@ class Student
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
+
   end
   
   def save
+
     sql = <<-SQL
       INSERT INTO students (name, grade) 
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
+
   end
   
   def self.create_table
+
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
@@ -63,11 +69,14 @@ class Student
     SQL
 
     DB[:conn].execute(sql)
+
   end
 
   def self.drop_table
+
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+
   end
 
   def self.all_students_in_grade_9
@@ -80,6 +89,7 @@ class Student
     SQL
 
     DB[:conn].execute(sql)
+
   end
 
   def self.students_below_12th_grade
@@ -94,6 +104,7 @@ class Student
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end 
+
   end
 
   def self.first_X_students_in_grade_10(n)
@@ -119,5 +130,37 @@ class Student
     DB[:conn].execute(sql, n).map do |row|
       self.new_from_db(row)
     end
+
   end
+
+  def self.first_student_in_grade_10
+    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+
+  end
+
+  def self.all_students_in_grade_X(grade)
+
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+
+  end
+
 end
